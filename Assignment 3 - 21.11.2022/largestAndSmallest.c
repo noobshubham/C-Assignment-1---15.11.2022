2c0ef3c423fa90360687165d872467f1e1818b5d
noobshubham <noobshubham65@gmail.com>
2022-11-22 13:20:07 +0530

    fix: formatting

--- Assignment 3 - 21.11.2022/largestAndSmallest.c
+++ Assignment 3 - 21.11.2022/largestAndSmallest.c
@@ -1,23 +1,27 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 
-void main(){
-    int n=0,largest=0,smallest=0,ck=0;
+void main()
+{
+    int n = 0, largest = 0, smallest = 0, ck = 0;
     printf("Enter the number: \n");
-    scanf("%d",&n);
-    largest = smallest =n % 10;
-    n=n/10;
-    while(n>0){
-        ck=n%10;
-        printf("###start ck: %d L: %d S: %d \n",ck,largest,smallest);
-        if(ck>largest){
-            largest=ck;
-            }
-        else if(ck<smallest){
-            smallest=ck;
-            }
-            printf("###ends ck: %d L: %d S: %d \n", ck, largest, smallest);
-            n = n / 10;
+    scanf("%d", &n);
+    largest = smallest = n % 10;
+    n = n / 10;
+    while (n > 0)
+    {
+        ck = n % 10;
+        printf("###start ck: %d L: %d S: %d \n", ck, largest, smallest);
+        if (ck > largest)
+        {
+            largest = ck;
+        }
+        else if (ck < smallest)
+        {
+            smallest = ck;
+        }
+        printf("###ends ck: %d L: %d S: %d \n", ck, largest, smallest);
+        n = n / 10;
     }
-    printf("Largest: %d\nSmallest: %d",largest,smallest);
+    printf("Largest: %d\nSmallest: %d", largest, smallest);
 }
