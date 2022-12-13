9b0caf840ce9d6c70a0a630d31d7a368e6cdd301
noobshubham <noobshubham65@gmail.com>
2022-12-13 14:02:38 +0530

    add: Stack Operation

---
+++ DS Assignment 1 - 13.12.2022 by Ms K Komala Devi/stack_n_its_operation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+
+struct stack
+{
+    int data;
+    int size;
+};
+
+void push(int stack[], int val)
+{
+}
+
+void pop() {}
+
+void main()
+{
+    int arr[SIZE];
+}
