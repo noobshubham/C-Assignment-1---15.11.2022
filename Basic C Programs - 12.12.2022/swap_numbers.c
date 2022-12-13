a6056d2de89753a52251498b66919dd115f6b041
noobshubham <noobshubham65@gmail.com>
2022-12-13 23:02:53 +0530

    add: Swap w/o 3rd Variable

---
+++ Basic C Programs - 12.12.2022/swap_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b;
+    printf("Enter a Value: ");
+    scanf("%d", &a);
+    printf("Enter b Value: ");
+    scanf("%d", &b);
+    a = b - a;
+    b = b - a;
+    a = a + b;
+    printf("Swapped Value for a & b are %d & %d respectively.", a, b);
+}
