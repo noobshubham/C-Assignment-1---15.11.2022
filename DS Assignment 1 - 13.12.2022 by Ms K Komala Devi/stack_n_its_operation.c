253605c4b768ee4794d46d6259b5ae67229501a9
noobshubham <noobshubham65@gmail.com>
2022-12-13 18:45:17 +0530

    add: Operations of Stack

--- DS Assignment 1 - 13.12.2022 by Ms K Komala Devi/stack_n_its_operation.c
+++ DS Assignment 1 - 13.12.2022 by Ms K Komala Devi/stack_n_its_operation.c
@@ -1,20 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 10
 
 struct stack
 {
-    int data;
     int size;
+    int top;
+    int *arr;
 };
 
-void push(int stack[], int val)
+void create(struct stack *s)
 {
+    printf("Enter the Size: ");
+    scanf("%d", &s->size);
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
 }
 
-void pop() {}
+void push(struct stack *s, int val)
+{
+    if (s->top == s->size - 1)
+        printf("Stack Overflow\n");
+    else
+        s->arr[++s->top] = val;
+}
+
+int pop(struct stack *s)
+{
+    int x = -1;
+    if (s->top == -1)
+        printf("Stack Underflow\n");
+    else
+        x = s->arr[s->top--];
+    return x;
+}
+
+int isEmpty(struct stack s)
+{
+    if (s.top == -1)
+        return 1;
+    return 0;
+}
+
+int isFull(struct stack s)
+{
+    return s.top == s.size - 1;
+}
+
+int stackTop(struct stack s)
+{
+    if (!isEmpty(s))
+        return s.arr[s.top];
+    return -1;
+}
+
+int peek(struct stack s, int index)
+{
+    int x = -1;
+    if (s.top - index + 1 < 0)
+        printf("Invalid Index\n");
+    x = s.arr[s.top - index + 1];
+    return x;
+}
 
 void main()
 {
-    int arr[SIZE];
+    struct stack st;
+    create(&st);
+
+    push(&st, 20);
+    push(&st, 40);
+    push(&st, 60);
+    push(&st, 80);
+
+    printf("%d \n", peek(st, 1));
+    printf("%d \n", peek(st, 2));
+    printf("%d \n", peek(st, 3));
+    printf("%d \n", peek(st, 4));
 }
