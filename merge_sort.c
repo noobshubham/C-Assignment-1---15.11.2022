a92134669f1fc67c5a8acecb2b5fe6b7481e06bc
noobshubham <noobshubham65@gmail.com>
2022-12-21 11:46:54 +0530

    add: Merge Sort

---
+++ merge_sort.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int *arr, int s, int e)
+{
+
+    int mid = (s + e) / 2;
+
+    int len1 = mid - s + 1;
+    int len2 = e - mid;
+
+    int *first = (int *)malloc(len1 * sizeof(int));
+    int *second = (int *)malloc(len2 * sizeof(int));
+
+    // copy values
+    int mainArrayIndex = s;
+    for (int i = 0; i < len1; i++)
+    {
+        first[i] = arr[mainArrayIndex++];
+    }
+
+    mainArrayIndex = mid + 1;
+    for (int i = 0; i < len2; i++)
+    {
+        second[i] = arr[mainArrayIndex++];
+    }
+
+    // merge 2 sorted arrays
+    int index1 = 0;
+    int index2 = 0;
+    mainArrayIndex = s;
+
+    while (index1 < len1 && index2 < len2)
+    {
+        if (first[index1] < second[index2])
+        {
+            arr[mainArrayIndex++] = first[index1++];
+        }
+        else
+        {
+            arr[mainArrayIndex++] = second[index2++];
+        }
+    }
+
+    while (index1 < len1)
+    {
+        arr[mainArrayIndex++] = first[index1++];
+    }
+
+    while (index2 < len2)
+    {
+        arr[mainArrayIndex++] = second[index2++];
+    }
+}
+
+void mergeSort(int *arr, int s, int e)
+{
+
+    // base case
+    if (s >= e)
+    {
+        return;
+    }
+
+    int mid = (s + e) / 2;
+
+    // left part sort karna h
+    mergeSort(arr, s, mid);
+
+    // right part sort karna h
+    mergeSort(arr, mid + 1, e);
+
+    // merge
+    merge(arr, s, e);
+}
+
+void main()
+{
+
+    int arr[] = {20, 17, 33, 42, 85, 10, 22, 15};
+    int n = 8;
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
