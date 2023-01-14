3772ede6c079da63e7fd0da03a9038f329ca3414
noobshubham <noobshubham65@gmail.com>
2023-01-14 12:43:42 +0530

    u: MatchStick Game

--- Algorithms 2 - 11.01.2023/matchstick_game.c
+++ Algorithms 2 - 11.01.2023/matchstick_game.c
@@ -1,21 +1,42 @@
 /*
-Write a program for a matchstick game being played between the computer and a user. 
-Your program should ensure that the computer always wins. 
-Rules for the game are as follows: 
-- There are 21 matchsticks. 
-- The computer asks the player to pick 1, 2, 3, or 4 matchsticks. 
-- After the person picks, the computer does its picking. 
+Write a program for a matchstick game being played between the computer and a user.
+Your program should ensure that the computer always wins.
+Rules for the game are as follows:
+- There are 21 matchsticks.
+- The computer asks the player to pick 1, 2, 3, or 4 matchsticks.
+- After the person picks, the computer does its picking.
 - Whoever is forced to pick up the last matchstick loses the game.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void play() {}
+void main()
+{
+    printf("Lets play the matchstick game!\n");
+    int matchstick = 21, computer, user;
 
-void main() {
-    printf("Lets play the matchstick game!");
-    int matchstick = 21;
+    while (matchstick >= 1)
+    {
 
-    printf("Pick a matchstick from 1 - 4: ");
+        printf("Total Match Sticks is %d\n", matchstick);
+        printf("Pick a matchstick from 1 - 4: ");
+        scanf("%d", &user);
+
+        if (user > 4)
+        {
+            printf("Invalid Entry");
+            break;
+        }
+
+        computer = 5 - user;
+        printf("computer picks up the %d matchsticks!\n", computer);
+        matchstick = matchstick - user - computer;
+
+        if (matchstick == 1)
+        {
+            printf("\nYou have been lost via computer.");
+            break;
+        }
+    }
 }
