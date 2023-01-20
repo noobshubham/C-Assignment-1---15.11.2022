4f03b0f73732443ef2456f43476358a1a11bad77
noobshubham <noobshubham65@gmail.com>
2023-01-20 21:54:48 +0530

    u: B.Search, I.Sort & S.Sort

--- sorting_algos.c
+++ sorting_algos.c
@@ -1,39 +1,105 @@
 #include <stdio.h>
 
-void Swap(int *a, int *b)
+int BinarySearch(int arr[], int n, int key)
 {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
+    int start, end, mid;
+    start = 0;
+    end = n - 1;
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return -1;
 }
 
-void SelectionSort(int a[], int n)
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void BubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+        int flag = 0;
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                flag = 1;
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+        if (flag != 1)
+            break;
+    }
+}
+
+void RecursiveBubbleSort(int arr[], int n)
+{
+    if (n == 1)
+        return;
+    else
+    {
+        for (int i = 0; i < n; i++)
+            if (arr[i] > arr[i + 1])
+                swap(&arr[i], &arr[i + 1]);
+        RecursiveBubbleSort(arr, n - 1);
+    }
+}
+
+void InsertionSort(int arr[], int n)
+{
+    int i, j, val;
+    for (i = 1; i < n; i++)
+    {
+        val = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > val)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = val;
+    }
+}
+
+void SelectionSort(int arr[], int n)
 {
     int i, j, min_idx;
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n - 1; i++)
     {
         min_idx = i;
         for (j = i + 1; j < n; j++)
         {
-            if (a[j] < a[min_idx])
+            if (arr[j] < arr[min_idx])
                 min_idx = j;
         }
-        Swap(&a[i], &a[min_idx]);
+        swap(&arr[min_idx], &arr[i]);
     }
 }
 
-void Display(int a[], int n)
+void PrintArray(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
-        printf("%d ", a[i]);
+        printf("%d ", arr[i]);
     printf("\n");
 }
 
 void main()
 {
-    int size = 5;
-    int arr[] = {9, 2, 5, 1, 3};
-    SelectionSort(arr, size);
-    Display(arr, size);
+    int n = 5, arr[] = {50, 20, 10, 30, 40};
+    // RecursiveBubbleSort(arr, n);
+    // InsertionSort(arr, n);
+    SelectionSort(arr, n);
+    PrintArray(arr, n);
 }
