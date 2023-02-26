84c08620420ba88c7e9431353b37444bbd9200f5
noobshubham <noobshubham65@gmail.com>
2023-02-26 10:48:29 +0530

    u: Heap Sort Code

--- Sorting Algos/heap_sort.c
+++ Sorting Algos/heap_sort.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 
+void Heapify(int arr[], int n, int i)
+{
+    int largest, left, right;
+    largest = i;
+    left = 2 * i + 1;
+    right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) largest = left;
+    if (right < n && arr[right] > arr[largest]) largest = right;
+
+    if (largest != i)
+    {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        Heapify(arr, n, largest);
+    }
+}
