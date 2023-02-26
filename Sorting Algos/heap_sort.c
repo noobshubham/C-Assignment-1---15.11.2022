156b6f4b0b1f06bb7a09a1b86ad4440421cef720
noobshubham <noobshubham65@gmail.com>
2023-02-26 10:57:57 +0530

    u: Heap Sort Code

--- Sorting Algos/heap_sort.c
+++ Sorting Algos/heap_sort.c
@@ -7,8 +7,10 @@ void Heapify(int arr[], int n, int i)
     left = 2 * i + 1;
     right = 2 * i + 2;
 
-    if (left < n && arr[left] > arr[largest]) largest = left;
-    if (right < n && arr[right] > arr[largest]) largest = right;
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
 
     if (largest != i)
     {
@@ -18,4 +20,43 @@ void Heapify(int arr[], int n, int i)
 
         Heapify(arr, n, largest);
     }
+}
+
+void HeapSort(int arr[], int n)
+{
+    int i;
+
+    // to make the arr into Max-Heap for the first time before sorting
+    for (i = n / 2 - 1; i >= 0; i--)
+        Heapify(arr, n, i);
+
+    // swap the root node with right-most leaf (i)
+    for (i = n - 1; i >= 0; i--)
+    {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        Heapify(arr, i, 0);
+    }
+}
+
+void Display(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void main() {
+    int arr[] = {81, 89, 9, 11, 14, 76, 54, 22};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Display the unsorted array first
+    Display(arr, n);
+
+    // Sort the Array using Heap Sort
+    HeapSort(arr, n);
+
+    // print the sorted array
+    Display(arr, n);
 }
