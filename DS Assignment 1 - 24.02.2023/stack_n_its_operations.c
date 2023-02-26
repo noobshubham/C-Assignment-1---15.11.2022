3f49f9cbaa344656fe47208ab0e5b18fd7402999
noobshubham <noobshubham65@gmail.com>
2023-02-26 11:39:19 +0530

    u: Stack  & its Operations

--- DS Assignment 1 - 24.02.2023/stack_n_its_operations.c
+++ DS Assignment 1 - 24.02.2023/stack_n_its_operations.c
@@ -1,1 +1,58 @@
-#in
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+void create(struct stack *s)
+{
+    printf("Enter the Size of the Array: ");
+    scanf("%d", s->size);
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
+}
+
+void push(struct stack *s, int val)
+{
+    if (s->top == s->size - 1) printf("Stack Overflow\n");
+    else
+        s->arr[++s->top] = val;
+}
+
+int pop(struct stack *s)
+{
+    int val = -1;
+    if (s->top == -1) printf("Stack Underflow\n");
+    else
+        val = s->arr[s->top--];
+    return val;
+}
+
+int isEmpty(struct stack s)
+{
+    if (s.top == -1) return 1;
+    return 0;
+}
+
+int isFull(struct stack s)
+{
+    return s.top == s.size - 1;
+}
+
+int stackTop(struct stack s)
+{
+    if (!isEmpty(s)) return s.arr[s.top];
+    return -1;
+}
+
+int peek(struct stack s, int index)
+{
+    int val = -1;
+    if (s.top - index + 1 < 0) printf("Invalid Index\n");
+    val = s.arr[s.top - index + 1];
+    return val;
+}
