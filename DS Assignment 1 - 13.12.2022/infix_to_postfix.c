7ce325b3687e5ac91c16da00a8a6c949d7d6fd3d
noobshubham <noobshubham65@gmail.com>
2023-02-27 23:45:47 +0530

    u: Infix to Postfix Code

--- DS Assignment 1 - 13.12.2022/infix_to_postfix.c
+++ DS Assignment 1 - 13.12.2022/infix_to_postfix.c
@@ -5,7 +5,42 @@ Rules of Infix to Post:
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 #include<ctype.h>
 
+int Precedence(char operator) {
+    switch (operator)
+    {
+        case '-':
+        case '+':
+            return 1;
+            break;
+        
+        case '*':
+        case '/':
+        case '%':
+            return 2;
+            break;
+        
+        case '^':
+            return 3;
+            break;
+    
+    default:
+        return -1;
+        break;
+    }
+}
 
+void InfixToPostfix(char *infix) {
+    int i, j, len;
+    len = strlen(infix);
+
+    for (i = 0; i < len; i++) {
+        if (isalnum(infix[i]) == 1) printf("%c", infix[i]);
+        else if (isalnum(infix[i] == 0)) {
+            While (Precedence(infix[i]) )
+        }
+    }
+}
 
